e4c76d4844757dd63311f88e9dd448d73be657c6
seanfitz922 <seanfitz922@gmail.com>
2023-10-08 16:49:35 -0500

    add train_dqn and dqn_model

--- dqn_model.py
+++ dqn_model.py
@@ -1,15 +1,25 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 # Define Deep Q-Network model
 class DQN(nn.Module):
     def __init__(self, input_size, output_size):
         super(DQN, self).__init__()
-        # Define nn archic
-        pass
+        
+        # Define neural network architecture
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size)
 
     def forward(self, x):
-        # Forward pass of your neural network
-        pass
+        # Forward pass 
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        q_values = self.fc3(x)
+        return q_values
 
-    def save(self, file_name = ''):
-        pass
+    def save(self, file_name='models/my_dqn_model.pth'):
+        # Save the model's state_dict to a file
+        if file_name:
+            torch.save(self.state_dict(), file_name)
--- train_dqn.py
+++ train_dqn.py
@@ -1,9 +1,18 @@
 import numpy as np
 import pandas as pd
-from collections import deque
 from custom_env import CustomCryptoTradingEnv
 from dqn_agent import DQNAgent
 
+"""
+to-do:
+    -implement exploration rate decay for episilon
+    -(done) add stopping criteria instead of fixed 1000 episodes
+    -tune hyper parameters
+    -ability to save trained model
+    -add data logging as opposed to print(info)
+"""
+
+# Define hyperparameters
 learning_rate = 0.001
 discount_factor = 0.99
 exploration_rate = 0.1
@@ -16,7 +25,6 @@ def train():
     df = pd.read_csv('bitcoin_2017_to_2023.csv', usecols=columns_to_load, parse_dates=['timestamp'])
     df.set_index('timestamp', inplace=True)
 
-    # resample and agg from https://stackoverflow.com/questions/47938372/pandas-dataframe-resample-aggregate-function-use-multiple-columns-with-a-customi
     # Resample the data to hourly intervals (OHLCV data)
     hourly_df = df.resample('1H').agg({
         'open': 'first',
@@ -28,10 +36,50 @@ def train():
 
     env = CustomCryptoTradingEnv(hourly_df)
 
-    # Create and train DQN agent
-    agent = DQNAgent(input_size='something', output_size='something')
-    agent.train(batch_size=batch_size)
+    # Create and initialize DQN agent
+    input_size = len(hourly_df.columns)  # open, high, low, close, volume
+    output_size = len(env.action_space)  # buy, sell, hold
+    agent = DQNAgent(input_size=input_size, output_size=output_size)
 
+    # Training loop
+    num_episodes = 1000  
+    # Desired profit threshold
+    target_profit = 1_000_000 
+    # Minimum acceptable balance
+    min_balance = 1_000 
+    # Initial balance
+    initial_balance = 10_000
+
+    for _ in range(num_episodes):
+        observation = env.reset()
+        done = False
+        episode_reward = 0.0  # Initialize episode reward
+        episode_balance = initial_balance  # Initialize episode balance
+
+        while not done:
+            # Choose an action using your DQN agent's policy
+            action = agent.select_action(observation)
+
+            # Take a step in the environment
+            new_observation, reward, done, info = env.step(action)
+
+            # Store the experience in the agent's replay memory
+            agent.remember(observation, action, reward, new_observation, done)
+
+            # Train the agent using experience replay 
+            agent.train(batch_size=batch_size)
+
+            # Update the current observation
+            observation = new_observation
+
+            episode_reward += reward
+            episode_balance += reward 
+
+            # Stopping condition: stop if the agent achieves the target profit or goes below the minimum balance
+            if episode_reward >= target_profit or episode_balance < min_balance:
+                done = True
+
+            print(info)
 
 if __name__ == '__main__':
     train()
